291581be97975c4f451a45a971ba9829ea3bba27
Qasdanovic <oussqas2002@gmail.com>
2025-01-08 22:53:30 +0100

    create a composer autoload

--- public/index.php
+++ public/index.php
@@ -8,15 +8,17 @@ const BASE_PATH = __DIR__."/../";
 
 
 require BASE_PATH."functions/function.php";
+
+require base_path("vendor/autoload.php");
 // require base_path("Database.php");
 
 
 
-spl_autoload_register(function($class){
-    $class = str_replace('\\', '/', $class);
-    // dd($class);
-    require base_path($class . ".php");
-});
+//spl_autoload_register(function($class){
+//    $class = str_replace('\\', '/', $class);
+//    // dd($class);
+//    require base_path($class . ".php");
+//});
 
 
 require base_path("bootstrap.php");
